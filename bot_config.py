6310d26aa1adb860950827b0ef566d940e15b267
cryptic-code <50461557+cryptic-code@users.noreply.github.com>
2021-07-10 12:16:00 +0530

    add dispatcher

---
+++ bot_config.py
@@ -0,0 +1,85 @@
+from os import getenv
+from load_config import get_reply
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler, CallbackContext, MessageHandler, Filters
+import requests
+
+TOKEN = getenv('TELEGRAM_TOKEN')
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, None, workers=0)
+
+API_BASE_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/'
+
+def start_cmd_handler(update: Update, context: CallbackContext) -> None:
+    """ Handle `/start` command """
+
+    reply = get_reply('START_MSG')
+    context.bot.send_message(chat_id=update.effective_message.chat_id, text=reply, parse_mode='markdown')
+
+def define_cmd_handler(update: Update, context: CallbackContext) -> None:
+    """ Handle /define coommand """
+
+    reply = get_reply('DEFINE_ERR')
+    context.bot.send_message(chat_id=update.effective_message.chat_id, text=reply, parse_mode='markdown')
+
+def help_cmd_handler(update: Update, context: CallbackContext) -> None:
+    """ Handle /help command """
+
+    reply = get_reply('HELP_MSG')
+    context.bot.send_message(chat_id=update.effective_message.chat_id, text=reply, parse_mode='markdown')
+
+def dict_req_handler(update: Update, context: CallbackContext) -> None:
+    """ Handle `define <word>` message """
+
+    chat_id = update.effective_message.chat_id
+    msg_txt = update.effective_message.text.encode('utf-8').decode()
+    _, search_key = msg_txt.lower().split("define ")
+
+    if len(search_key.split(' ')) > 1:
+        context.bot.send_message(chat_id=chat_id, text=get_reply('WORD_ERR'))
+    else:
+
+        search_url = API_BASE_URL + search_key
+        response = requests.get(search_url)
+
+        if response: #check if response is valid 200-400
+            data = response.json()[0]
+
+            defined_word = data['word']
+            phonetic_txt = data['phonetics'][0]['text']
+            phonetic_audio = data['phonetics'][0]['audio']
+
+            context.bot.send_message(chat_id=chat_id, text=get_reply('PHONETICS_MSG', {'defined_word': defined_word, 'phonetic_txt': phonetic_txt, 'phonetic_audio': phonetic_audio}), parse_mode='markdown')
+
+            for object_ in data['meanings']:
+                part_of_speech = object_['partOfSpeech']
+                definition = object_['definitions'][0]['definition']
+                example = str(object_['definitions'][0].get('example'))
+
+                context.bot.send_message(chat_id=chat_id, text=get_reply('DEFINITION_MSG', {'part_of_speech': part_of_speech, 'definition': definition, 'example': example}), parse_mode='markdown')
+        else:
+            context.bot.send_message(chat_id=chat_id, text=get_reply("WORD_ERR"), parse_mode="markdown")
+
+def edits_handler(update: Update, context: CallbackContext) -> None:
+    """ Handle edited messages. """
+
+    context.bot.send_message(chat_id=update.effective_message.chat_id, text=get_reply('EDITS_REPLY'), parse_mode='markdown')
+
+def unrecognized_handler(update: Update, context: CallbackContext) -> None:
+    """ Handle unrecognized messages. """
+
+    context.bot.send_message(chat_id=update.effective_message.chat_id, text=get_reply('UNRECOGNIZED'), parse_mode='markdown')
+
+def attachment_handler(update: Update, context: CallbackContext) -> None:
+    """ Handle attachments. """
+    
+    context.bot.send_message(chat_id=update.effective_message.chat_id, text=get_reply('ATTACHMENTS_ERR'), parse_mode='markdown')
+
+dp.add_handler(CommandHandler(command='start', callback=start_cmd_handler, filters=(~Filters.update.edited_message)))
+dp.add_handler(CommandHandler(command='define', callback=define_cmd_handler, filters=(~Filters.update.edited_message)))
+dp.add_handler(CommandHandler(command='help', callback=help_cmd_handler, filters=(~Filters.update.edited_message)))
+dp.add_handler(MessageHandler(filters=(Filters.text & (~Filters.command) & (~Filters.update.edited_message)), callback=dict_req_handler))
+dp.add_handler(MessageHandler(filters=(Filters.update.edited_message), callback=edits_handler))
+dp.add_handler(MessageHandler(filters=(Filters.attachment), callback=attachment_handler))
+dp.add_error_handler(unrecognized_handler)
--- main.py
+++ main.py
@@ -1,73 +1,18 @@
-import os
+from os import getenv
 from custom_logging import get_logger
-from load_config import get_reply
+from bot_config import bot, dp
+from telegram import Update
 from dotenv import load_dotenv
-import requests
-import telegram
 from flask import Flask, request
 
 load_dotenv()
 
-TOKEN = os.getenv('TELEGRAM_TOKEN')
-APP_URL = os.getenv('APP_URL')
-ADMIN_PASS = os.getenv('ADMIN_PASS')
-
-API_BASE_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/'
+APP_URL = getenv('APP_URL')
+ADMIN_PASS = getenv('ADMIN_PASS')
 
 logger = get_logger(__name__, only_debug=True)
 
-bot = telegram.Bot(token=TOKEN)
 app = Flask(__name__)
-
-def start_cmd_handler(chat_id: telegram.Chat.id) -> None:
-    reply = get_reply('START_MSG')
-    bot.send_message(chat_id=chat_id, text=reply, parse_mode='markdown')
-
-def define_cmd_handler(chat_id: telegram.Chat.id) -> None:
-    reply = get_reply('DEFINE_ERR')
-    bot.send_message(chat_id=chat_id, text=reply, parse_mode='markdown')
-
-def help_cmd_handler(chat_id: telegram.Chat.id) -> None:
-    reply = get_reply('HELP_MSG')
-    bot.send_message(chat_id=chat_id, text=reply, parse_mode='markdown')
-
-def dict_req_handler(msg_txt: str, chat_id: telegram.Chat.id) -> None:
-    _, search_key = msg_txt.lower().split("define ")
-
-    if len(search_key.split(' ')) > 1:
-        bot.send_message(chat_id=chat_id, text=get_reply('WORD_ERR'))
-    else:
-        logger.info(f"Fetching definition for {search_key}")
-
-        search_url = API_BASE_URL + search_key
-        response = requests.get(search_url)
-
-        if response:
-            data = response.json()[0]
-
-            defined_word = data['word']
-            phonetic_txt = data['phonetics'][0]['text']
-            phonetic_audio = data['phonetics'][0]['audio']
-
-            bot.send_message(chat_id=chat_id, text=get_reply('PHONETICS_MSG', {'defined_word': defined_word, 'phonetic_txt': phonetic_txt, 'phonetic_audio': phonetic_audio}), parse_mode='markdown')
-
-            for object_ in data['meanings']:
-                part_of_speech = object_['partOfSpeech']
-                definition = object_['definitions'][0]['definition']
-                example = str(object_['definitions'][0].get('example'))
-
-                bot.send_message(chat_id=chat_id, text=get_reply('DEFINITION_MSG', {'part_of_speech': part_of_speech, 'definition': definition, 'example': example}), parse_mode='markdown')
-        else:
-            bot.send_message(chat_id=chat_id, text=get_reply("WORD_ERR"), parse_mode="markdown")
-
-def edits_handler(chat_id: telegram.Chat.id):
-    bot.send_message(chat_id=chat_id, text=get_reply('EDITS_REPLY'), parse_mode='markdown')
-
-def unrecognized_handler(chat_id: telegram.Chat.id):
-    bot.send_message(chat_id=chat_id, text=get_reply('UNRECOGNIZED'), parse_mode='markdown')
-
-def attachment_handler(chat_id: telegram.Chat.id):
-    bot.send_message(chat_id=chat_id, text=get_reply('ATTACHMENTS_ERR'), parse_mode='markdown')
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
@@ -89,37 +34,8 @@ def index():
 @app.route('/telegram-dict', methods=["POST", "GET"])
 def handle_request():
     if request.method == 'POST':
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-        chat_id = update.effective_chat.id
-        
-        if update.edited_message:
-            edits_handler(chat_id)
-            return "OK"
-
-        if update.chat_member or update.my_chat_member:
-            return "OK"
-
-        if update.effective_message.effective_attachment:
-            attachment_handler(chat_id)
-            return "OK"
-
-        msg_id = update.effective_message.message_id
-        msg_txt = update.message.text.encode('utf-8').decode()
-
-        logger.info(f"Received message: {msg_txt}")
-
-        if msg_txt == '/start':
-            start_cmd_handler(chat_id)
-        elif msg_txt.startswith('/define'):
-            define_cmd_handler(chat_id)
-        elif msg_txt.startswith('/help'):
-            help_cmd_handler(chat_id)
-        elif msg_txt.lower().startswith('define '):
-            dict_req_handler(msg_txt, chat_id)
-        else:
-            unrecognized_handler(chat_id)
-
+        update = Update.de_json(request.get_json(force=True), bot)
+        dp.process_update(update)
         return "OK"
     else:
         return "Hello, world! ;)"
